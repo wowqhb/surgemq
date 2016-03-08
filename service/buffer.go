9041769f60ca2a39d51c4135056763399f0d767c
axe <13821056120@139.com>
2016-03-08 15:46:47 +0800

    修改了buffer.go;sendrecv.go;process.go

--- service/buffer.go
+++ service/buffer.go
@@ -243,7 +243,7 @@ func (this *buffer) ReadFrom(r io.Reader) (int64, error) {
 			return total, err
 		}
 		pstart := start & this.mask
-		this.buf[pstart] = b__[0:]
+		this.buf[pstart] = &b__
 		_, err = this.WriteCommit(int(total) /*n*/)
 		if err != nil {
 			return total, err
@@ -337,7 +337,7 @@ func (this *buffer) Read(p []byte) (int, error) {
 		//    The number of bytes will NOT be len(p) but less than that.
 		//if cpos+n < ppos {
 		if cpos < ppos {
-			n := copy(p, this.buf[cindex][0:])
+			n := copy(p, *(this.buf[cindex]))
 
 			this.cseq.set(cpos + int64(1 /*n*/))
 			this.pcond.L.Lock()
@@ -406,7 +406,7 @@ func (this *buffer) Write(p []byte) (int, error) {
 	//total := ringCopy(*(this.buf[start]), p, int64(start)&this.mask)
 	//p_ := make([]byte, 0, len(p))
 	//p_ = append(p_, p[0:]...)
-	this.buf[int64(start)&this.mask] = p
+	this.buf[int64(start)&this.mask] = &p
 	this.pseq.set(start + int64(1))
 	this.ccond.L.Lock()
 	this.ccond.Broadcast()
@@ -423,7 +423,7 @@ func (this *buffer) Write(p []byte) (int, error) {
 // b's buffer size.
 // If there's not enough data to peek, error is ErrBufferInsufficientData.
 // If n < 0, error is bufio.ErrNegativeCount
-func (this *buffer) ReadPeek(n int) ([]byte, error) {
+func (this *buffer) ReadPeek(n int) (*[]byte, error) {
 	defer Log.Debugc(func() string {
 		return fmt.Sprintf("ReadPeek 结束")
 	})
@@ -492,7 +492,7 @@ func (this *buffer) ReadPeek(n int) ([]byte, error) {
 // Wait waits for for n bytes to be ready. If there's not enough data, then it will
 // wait until there's enough. This differs from ReadPeek or Readin that Peek will
 // return whatever is available and won't wait for full count.
-func (this *buffer) ReadWait(n int) ([]byte, error) {
+func (this *buffer) ReadWait(n int) (*[]byte, error) {
 	Log.Debugc(func() string {
 		return fmt.Sprintf("ReadWait 开始执行")
 	})
@@ -583,7 +583,7 @@ func (this *buffer) ReadCommit(n int) (int, error) {
 // 1. the slice pointing to the location in the buffer to be filled
 // 2. a boolean indicating whether the bytes available wraps around the ring
 // 3. any errors encountered. If there's error then other return values are invalid
-func (this *buffer) WriteWait(n int) ([]byte, bool, error) {
+func (this *buffer) WriteWait(n int) (*[]byte, bool, error) {
 	start, _, err := this.waitForWriteSpace(n /*n*/)
 	if err != nil {
 		return nil, false, err
