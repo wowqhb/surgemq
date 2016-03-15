d9737da770f8ec3bd1be529ea49cb464c51be1e1
wowqhb <13821056120@139.com>
2016-03-15 17:36:35 +0800

    修改 buffer.go process.go sendrecv.go buffer modify sleep 5 Microsecond modify buffre.go readfrom sleep 5毫秒

--- service/buffer.go
+++ service/buffer.go
@@ -26,13 +26,13 @@ import (
 )
 
 var (
-	bufcnt            int64
+	bufcnt int64
 	DefaultBufferSize int64
 
-	DeviceInBufferSize  int64
+	DeviceInBufferSize int64
 	DeviceOutBufferSize int64
 
-	MasterInBufferSize  int64
+	MasterInBufferSize int64
 	MasterOutBufferSize int64
 )
 
@@ -60,19 +60,19 @@ func (this *sequence) set(seq int64) {
 }
 
 type buffer struct {
-	id int64
+	id         int64
 
 	readIndex  int64 //读序号
 	writeIndex int64 //写序号
 	buf        []*[]byte
 
-	size int64
-	mask int64
+	size       int64
+	mask       int64
 
-	done int64
+	done       int64
 
-	pcond *sync.Cond
-	ccond *sync.Cond
+	pcond      *sync.Cond
+	ccond      *sync.Cond
 }
 
 func newBuffer(size int64) (*buffer, error) {
@@ -188,7 +188,7 @@ func (this *buffer) WriteBuffer(in *[]byte) (ok bool) {
 			return false
 		}
 		readIndex = this.GetCurrentReadIndex()
-		if writeIndex >= readIndex && writeIndex-readIndex >= this.size {
+		if writeIndex >= readIndex && writeIndex - readIndex >= this.size {
 			//fmt.Println("write wait")
 			this.ccond.Broadcast()
 			this.pcond.Wait()
@@ -245,16 +245,15 @@ func (this *buffer) ReadFrom(r io.Reader) (int64, error) {
 				break
 			}
 		}
-		remlen, m := binary.Uvarint(b[1 : max_cnt+1])
+		remlen, m := binary.Uvarint(b[1 : max_cnt + 1])
 		remlen_tmp := int64(remlen)
 		total_tmp := remlen_tmp + int64(1) + int64(m)
 
-		write_bytes = make([]byte, total_tmp)
-		write_bytes = append(write_bytes[0:], b[0:m+1]...)
+		write_bytes = make([]byte, 0, total_tmp)
+		write_bytes = append(write_bytes, b[0:m + 1]...)
 		nlen := int64(0)
 		times := 0
 		cnt_ := 32
-		start := int64(1) + int64(m)
 		for nlen < remlen_tmp {
 			if this.isDone() {
 				return total, io.EOF
@@ -267,21 +266,15 @@ func (this *buffer) ReadFrom(r io.Reader) (int64, error) {
 			times++
 			tmpm := remlen_tmp - nlen
 
-			len := int64(cnt_)
+			var b_ []byte
 			if tmpm < int64(cnt_) {
-				len = tmpm
-			}
-			//var b_ []byte
-			/*if tmpm < int64(cnt_) {
 				b_ = make([]byte, tmpm)
 			} else {
 				b_ = make([]byte, cnt_)
-			}*/
+			}
 
 			//b_ := make([]byte, remlen)
-			//n, err = r.Read(b_[0:])
-			b_ := write_bytes[start+nlen : start+nlen+len]
-			n, err = r.Read(b_)
+			n, err = r.Read(b_[0:])
 
 			if err != nil {
 				/*Log.Errorc(func() string {
@@ -291,7 +284,7 @@ func (this *buffer) ReadFrom(r io.Reader) (int64, error) {
 				continue*/
 				return total, err
 			}
-			//write_bytes = append(write_bytes, b_[0:]...)
+			write_bytes = append(write_bytes, b_[0:]...)
 			nlen += int64(n)
 			total += int64(n)
 		}
@@ -359,7 +352,7 @@ func ringCopy(dst, src []byte, start int64) int {
 }
 
 func powerOfTwo64(n int64) bool {
-	return n != 0 && (n&(n-1)) == 0
+	return n != 0 && (n & (n - 1)) == 0
 }
 
 func roundUpPowerOfTwo64(n int64) int64 {
